94a52c9f832397d2c21219c609fce641afdd9219
davidramirezm30 <davidramirez.drm@gmail.com>
2023-07-13 18:03:25 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+### Alexa Youtube Music Skill
+
+Alexa does not provide native integration to neither youtube nor youtube music. This skill helps to play music from
+youtube music on your amazon echo device
+
+NOTE: The skill will not be published for public use as it can incur charges due to AWS Lambda usage.  Users who
+are looking to install this skill on their echo device should have AWS developer account and then use the skill
+
+Anyone is free to publish the skill in their AWS account so that non-tech savvy users can also make use of it. If there
+is some skill that is available please raise a PR so that, the link to the skill can be added here
+
+#### Setting up the skill
+1. You need to have NodeJS and ```npm``` installed.  
+You can download NodeJS for your platform and follow instructions from [https://nodejs.org/en/download/](https://nodejs.org/en/download/)
+
+2. You need an [AWS account](https://aws.amazon.com) and an [Amazon developer account](https://developer.amazon.com) to create an Alexa Skill.
+
+3. You need to install and configure the [AWS CLI](https://aws.amazon.com/cli/)
+
+4. You need to install and to initialize [ASK CLI](https://developer.amazon.com/docs/smapi/quick-start-alexa-skills-kit-command-line-interface.html) with
+
+5. Generate a google cloud API with `YouTube Data API v3` restriction, and add a it to the lambda function as an environment variable with the name `YOUTUBE_API_KEY`
+
+### Deployment
+
+ASK will create the skill and the lambda function for you.
+
+Lambda function will be created in ```eu-west-1```.
+
+You deploy the skill and the lambda function in one step :
+
+```bash
+$ ask deploy
+```
+
+## Cleanup
+
+If you were deploying this skill just for learning purposes or for testing, do not forget to clean your AWS account to avoid recurring charges for your DynamoDB table.
+
+- delete the lambda function (ask-custom-Multi_Stream_Audio_Player-default)
+- delete the IAM execution role (ask-lambda-Multi-Stream-Audio-Player)
+- delete the DynamoDB table (LongFormAudioSample)
---
+++ lambda/index.js
@@ -0,0 +1,181 @@
+const Alexa = require('ask-sdk-core');
+const ytmusic = require('node-youtube-music');
+const ytlist = require('yt-list');
+const ytdl = require('ytdl-core');
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Bienvenido a Youtube Music';
+        const repromptSpeakOutput = 'Puedes decir, reproduce Linkin Park, para comenzar'
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(repromptSpeakOutput)
+            .getResponse();
+    }
+};
+const PlaySongIntentHandler = {
+    async canHandle(handlerInput) {
+        return (
+            Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+            Alexa.getIntentName(handlerInput.requestEnvelope) === "PlaySongIntent"
+        );
+    },
+    handle(handlerInput) {
+        console.log("Play music");
+        const speechText =
+            handlerInput.requestEnvelope.request.intent.slots.songQuery.value;
+        if (speechText) {
+            return controller.search(handlerInput, speechText);
+        } else {
+            return handlerInput.responseBuilder
+                .speak("Para comenzar puedes decir, reproduce Linkin Park.")
+                .getResponse();
+        }
+    },
+
+};
+
+const controller = {
+    async search(handlerInput, query) {
+      console.log(query);
+      const data = await searchForVideos(query);
+      return this.play(handlerInput, data.items[0]);
+    },
+    async play(handlerInput, audioInfo) {
+      const { responseBuilder } = handlerInput;
+      const playBehavior = "REPLACE_ALL";
+      console.log("play");
+      console.log(audioInfo);
+      const id= audioInfo.id.videoId;
+      const audioFormat = await getAudioUrl(id);
+      console.log(audioFormat);
+      responseBuilder
+        .speak(`Reproduciendo  ${audioInfo.snippet.title}`)
+        .withShouldEndSession(true)
+        .addAudioPlayerPlayDirective(
+          playBehavior,
+          audioFormat.url,
+          audioInfo.id.videoId,
+          0,
+          null
+        );
+      return responseBuilder.getResponse();
+    },
+    async stop(handlerInput, message) {
+        return handlerInput.responseBuilder
+            .speak(message)
+            .addAudioPlayerStopDirective()
+            .getResponse();
+    },
+};
+
+const searchForMusic = async (searchQuery) => {
+    musics = await ytmusic.searchMusics(searchQuery);
+    return musics[0];
+
+}
+
+const searchForVideos = async (searchQuery, nextPageToken, amount) => {
+    return await ytlist.searchVideos(searchQuery, nextPageToken, amount);
+}
+
+const getAudioUrl = async (videoId) => {
+    const audioInfo = await ytdl.getInfo(videoId, {});
+    const audioFormat = await ytdl.chooseFormat(audioInfo.formats, {
+        quality: "highestaudio",
+    });
+    return audioFormat;
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = '¡Puedes saludarme! ¿Cómo puedo ayudarte?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.PauseIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = '¡Adiós!';
+        return controller.stop(handlerInput, speakOutput);
+    }
+};
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse();
+    }
+};
+
+// The intent reflector is used for interaction model testing and debugging.
+// It will simply repeat the intent the user said. You can create custom handlers
+// for your intents by defining them above, then also adding them to the request
+// handler chain below.
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `Acabas de activar ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+// Generic error handling to capture any syntax or routing errors. If you receive an error
+// stating the request handler chain is not found, you have not implemented a handler for
+// the intent being invoked or included it in the skill builder below.
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`~~~~ Error handled: ${error.stack}`);
+        const speakOutput = `Lo siento, tuve problemas para hacer lo que me pediste. Inténtalo de nuevo.`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+// The SkillBuilder acts as the entry point for your skill, routing all request and response
+// payloads to the handlers above. Make sure any new handlers or interceptors you've
+// defined are included below. The order matters - they're processed top to bottom.
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        PlaySongIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
+        ) 
+    .addErrorHandlers(
+        ErrorHandler,
+        )
+    .lambda();
